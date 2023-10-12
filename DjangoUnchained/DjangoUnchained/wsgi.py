6569ca07eb43b8d516afef87bc43090eae90bc22
John Doe <sforde6776@gmail.com>
2023-10-12 14:45:08 -0400

    Initital Django Files

---
+++ DjangoUnchained/DjangoUnchained/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for DjangoUnchained project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoUnchained.settings')
+
+application = get_wsgi_application()
