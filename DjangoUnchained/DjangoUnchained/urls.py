488278bbbbc374b1e31f471009c03d0df3ecd046
kelseyzeng0610 <zengqd@bc.edu>
2023-10-19 15:41:48 -0400

    models in db ( user profile( user type), system states,person( abstract class), student, and Admin

--- DjangoUnchained/DjangoUnchained/urls.py
+++ DjangoUnchained/DjangoUnchained/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    
 ]
--- DjangoUnchained/EagleVision/admin.py
+++ DjangoUnchained/EagleVision/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.apps import apps
 
 # Register your models here.
+# @admin.register(Student)
+# class StudentAdmin(admin.ModelAdmin):
+#     list_display = ('eagle_id', 'major_1', 'major_2', 'major_3', 'minor_1', 'minor_2', 'graduation_semester')
+#     search_fields = ('eagle_id', 'major_1', 'minor_1')
+#     list_filter = ('graduation_semester',)
+
+app_models = apps.get_app_config('EagleVision').get_models()
+
+for model in app_models:
+    try:
+        admin.site.register(model)
+    except admin.sites.AlreadyRegistered:
+        pass
+
