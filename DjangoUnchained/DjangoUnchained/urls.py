eeaf6f1c7970f2ee49a81f67e04c0a0bcea96e2f
John Doe <sforde6776@gmail.com>
2023-10-20 11:43:49 -0400

    messed up git a little bit

--- DjangoUnchained/DjangoUnchained/urls.py
+++ DjangoUnchained/DjangoUnchained/urls.py
@@ -15,11 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    
+    path('', include('EagleVision.urls')),
 ]
