5081385c55a9105b9aa6bf222767bc7cb8d120a3
John Doe <sforde6776@gmail.com>
2023-10-20 11:25:29 -0400

    login stuff

--- DjangoUnchained/EagleVision/views.py
+++ DjangoUnchained/EagleVision/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
+
+def login(request):
+    template = loader.get_template('login.html')
+    context = {
+        'Title': 'Sign into your Account', 
+        'FieldOne': 'Email',
+        'FieldTwo': 'Password'
+    }
+    return HttpResponse(template.render(context, request))
