0a45071331823e6ce1bc3702ed2da2700a58f3e4
petya-stoeva <stoeva@bc.edu>
2023-10-21 18:34:22 -0400

    views profile

--- DjangoUnchained/EagleVision/views.py
+++ DjangoUnchained/EagleVision/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -12,3 +13,7 @@ def login(request):
         'FieldTwo': 'Password'
     }
     return HttpResponse(template.render(context, request))
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
