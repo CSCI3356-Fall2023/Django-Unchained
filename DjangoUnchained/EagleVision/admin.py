30040bad5244ab0b58a36d17bc72d10a0f11cdac
pluto-0 <adamaguiar10@gmail.com>
2023-10-21 11:24:55 -0400

    Added function to change open/closed

--- DjangoUnchained/EagleVision/admin.py
+++ DjangoUnchained/EagleVision/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+
+def change_open_closed(desired_state):
+    if desired_state.upper() != 'OPEN' and desired_state.upper() != 'CLOSED':
+        return
+        
+    current_state = {'OPEN' : 'CLOSED', 'CLOSED' : 'OPEN'}
+    search_text = f"OPEN_CLOSED = '{current_state[desired_state.upper()]}'"
+    replace_text = f"OPEN_CLOSED = '{desired_state.upper()}'"
+
+    with open(r'../DjangoUnchained/settings.py', 'r') as file:
+        data = file.read()
+        data = data.replace(search_text, replace_text)
+    
+    with open(r'../DjangoUnchained/settings.py', 'w') as file:
+        file.write(data)
