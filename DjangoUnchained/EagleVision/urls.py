a3c06661bcb43d104c2c4471882b771dd50ee908
John Doe <sforde6776@gmail.com>
2023-10-22 16:08:20 -0400

    more changes

--- DjangoUnchained/EagleVision/urls.py
+++ DjangoUnchained/EagleVision/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.login, name='login'), 
+    path('', views.forgot, name='forgotPassword'), 
+    path('', views.signup, name='signUp'),
+    path('', views.signin, name='signin')
 ]
--- DjangoUnchained/EagleVision/views.py
+++ DjangoUnchained/EagleVision/views.py
@@ -9,6 +9,30 @@ def login(request):
     context = {
         'Title': 'Sign into your Account', 
         'FieldOne': 'Email',
-        'FieldTwo': 'Password'
+        'FieldTwo': 'Password',
+        'Button': 'Login'
     }
     return HttpResponse(template.render(context, request))
+
+def forgot(request):
+    template = loader.get_template('login.html')
+    context = {
+        'Title': 'Reset Password', 
+        'FieldOne': 'Email',
+        'FieldTwo': 'New Password',
+        'Button': 'Confirm'
+    }
+    return HttpResponse(template.render(context, request))
+
+def signup(request):
+    template = loader.get_template('login.html')
+    context = {
+        'Title': 'Sign Up', 
+        'FieldOne': 'Email',
+        'FieldTwo': 'Password',
+        'Button': 'Sign Up'
+    }
+    return HttpResponse(template.render(context, request))
+
+def signin(request):
+    template = loader.get_template('landing.html')
