df704b42afedac80fa85c8f93c950ebd9c991b29
petya-stoeva <stoeva@bc.edu>
2023-10-22 20:59:51 -0400

    comma error

--- DjangoUnchained/EagleVision/urls.py
+++ DjangoUnchained/EagleVision/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', views.forgot, name='forgotPassword'), 
     path('register/', views.register, name='register'),
     path('register/student/', views.student_register, name='student_register'),
-    path('register/admin/', views.admin_register, name='admin_register'),,
+    path('register/admin/', views.admin_register, name='admin_register'),
     path('profile/', views.user_profile, name='profile')
 ]
