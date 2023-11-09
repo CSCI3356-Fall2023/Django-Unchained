afdb84be94d7f7169721fba690adc794b27b9d21
unknown <marcomao0719@gmail.com>
2023-11-08 19:08:13 -0500

    solved access blocked

--- DjangoUnchained/DjangoUnchained/settings.py
+++ DjangoUnchained/DjangoUnchained/settings.py
@@ -61,16 +61,13 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.google'
 ]
 
-#SOCIALACCOUNT_LOGIN_ON_GET=True
-
 SOCIALACCOUNT_PROVIDERS = {
     'google': {
-        'SCOPE': [
-            'profile',
-            'email',
-        ],
-        'AUTH_PARAMS': {
-            'access_type': 'online',
+        'APP': {
+            'client_id': '159123941533-972leu77ipdva2jn60hki4r5j0qquh3d.apps.googleusercontent.com',
+            'secret': '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',
+            'key': '',
+            'redirect_uris': ['http://127.0.0.1:8000/account/google/login/callback/'],
         }
     },
 }
@@ -89,7 +86,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    #'allauth.account.middleware.AccountMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'DjangoUnchained.urls'
@@ -168,4 +165,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 OPEN_CLOSED = 'OPEN'
 
 LOGIN_REDIRECT_URL = '/profile/'
-LOGOUT_REDIRECT_URL = '/'
+
