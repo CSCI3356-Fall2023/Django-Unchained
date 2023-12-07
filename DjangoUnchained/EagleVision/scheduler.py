de4799859cf61daf4928e84a0a9e514f57f3366b
petya-stoeva <stoeva@bc.edu>
2023-12-06 22:42:23 -0500

    Baackground Scheduler + Seat Updates

--- DjangoUnchained/EagleVision/scheduler.py
+++ DjangoUnchained/EagleVision/scheduler.py
@@ -3,21 +3,10 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from django.conf import settings
 from django.core.mail import send_mail
 from .models import Section, Watchlist
+from .scheduled_jobs import change_seats_job, check_and_notify
 
-def check_and_notify():
-    sections = Section.objects.all()
-
-    for section in sections:
-        watchlist_entries = Watchlist.objects.filter(section=section)
-
-        for watchlist_entry in watchlist_entries:
-            user = watchlist_entry.user
-            if section.currentSeats < section.maxSeats:
-                # Send email notification
-                subject = f'Seats Available for {section.title}'
-                message = f'There are {section.maxSeats - section.currentSeats} available seats for {section.title}.'
-                send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [user.email])
-
-scheduler = BackgroundScheduler()
-scheduler.add_job(check_and_notify, 'interval', minutes=60)  # Adjust the interval as needed
-scheduler.start()
+def run_schedule():
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(check_and_notify, 'cron', hour='6')  # Adjust the interval as needed
+    scheduler.add_job(change_seats_job, 'cron', hour='6')
+    scheduler.start()
