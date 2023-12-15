b9c62365b48f7c325c05f70ac43eedd2b4d4de05
John Doe <sforde6776@gmail.com>
2023-12-14 22:30:17 -0500

    deployment work

--- DjangoUnchained/DjangoUnchained/settings.py
+++ DjangoUnchained/DjangoUnchained/settings.py
@@ -41,9 +41,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pm3=454tqba&qi_!#%t*f$1^6pe310aw*+8yne7@2ur+@g3b4!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Application definition
@@ -62,6 +64,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
