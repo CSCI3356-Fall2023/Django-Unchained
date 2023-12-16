1aec198db41aa18141ece1abf0f5ab95e0bc02ae
kelseyzeng0610 <zengqd@bc.edu>
2023-12-16 16:32:36 -0500

    make sure it calls https instead of http

--- DjangoUnchained/DjangoUnchained/settings.py
+++ DjangoUnchained/DjangoUnchained/settings.py
@@ -31,6 +31,8 @@ AUTH0_CLIENT_ID = os.environ.get("AUTH0_CLIENT_ID")
 AUTH0_CLIENT_SECRET = os.environ.get("AUTH0_CLIENT_SECRET")
 AUTH0_REDIRECT_URI = 'https://django-unchained-production.up.railway.app/login'
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
